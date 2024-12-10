7ad2be806a193aa1d39092b70e235a31b14385e8
SGuzmanR <sguzmanr2@gmail.com>
2024-12-10 15:00:11 -0500

    Edited page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Portfolio />
-      <ContactMe />
+      {/* <Portfolio />
+      <ContactMe /> */}
     </main>
   );
 }
